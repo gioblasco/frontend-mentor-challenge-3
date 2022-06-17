3ae8a2a5573d66eef6f3e7c688b4aa8ccc4e1ab3
Giovanna Blasco <giovanna.martin@opus-software.com.br>
2022-06-17 17:54:16 -0300

    Styling components and adding font-family

--- ui/src/components/emailInput.tsx
+++ ui/src/components/emailInput.tsx
@@ -1,12 +1,53 @@
 import * as React from "react"
+import './emailInput.scss'
+import arrow from '../images/icon-arrow.svg';
 
-const EmailInput = () => {
-    return (
-        <div>
-            <input type="text" />
-            <button><img src="../images/icon-arrow.svg" alt="Submit button"/></button>
-        </div>
-    )
+interface IProps {
+}
+
+interface IState {
+    email: string;
+    emailError: boolean
+}
+
+class EmailInput extends React.Component<IProps, IState> {
+    state: IState = {
+        email: '',
+        emailError: false
+    }
+
+    onChange = (newValue: string) => {
+        this.setState({email: newValue, emailError: false})
+    }
+
+    onSubmit = (event: React.SyntheticEvent) => {
+        event.preventDefault();
+        if (this.isValidEmail(this.state.email)) {
+            alert('Email submitted successfully');
+            this.setState({email: '', emailError: false});
+            return;
+        }
+
+        this.setState({emailError: true});
+    }
+
+    isValidEmail = (email: string) => {
+        const emailRegex = new RegExp(
+            '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$'
+        );
+
+        return emailRegex.test(email);
+    }
+
+    render() {
+        return ( 
+            <form onSubmit={this.onSubmit} noValidate>
+                <input type="email" name="email" placeholder="Email Address" className={`email${this.state.emailError ? "-error" : ''}`} value={this.state.email} onChange={(e) => this.onChange(e.target.value)}/>
+                <button name="Submit" className="submit-button"><img src={arrow} alt="Arrow"/></button>
+                { this.state.emailError && <label className="label-error">Please provide a valid email</label> }
+            </form>
+        )
+    }
 }
 
 export default EmailInput
--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,14 +1,24 @@
 import * as React from "react"
 import EmailInput from "../components/emailInput"
+import logo from "../images/logo.svg";
+import "./index.scss";
+import "@fontsource/josefin-sans";
 
 const IndexPage = () => {
   return (
-    <main>
-      <title>We're coming soon</title>
-      <p>Hello fellow shoppers! We're currently building our new fashion store. 
-      Add your email below to stay up-to-date with announcements and our launch deals.</p>
-      <EmailInput />
-    </main>
+    <div className="container">
+      <main className="left-side">
+        <title>Coming soon</title>
+        <article>
+          <img src={logo} alt="Brand logo" />
+          <h1><span className="title brown">We're</span> <span className="title black">coming soon</span></h1>
+          <p>Hello fellow shoppers! We're currently building our new fashion store. 
+          Add your email below to stay up-to-date with announcements and our launch deals.</p>
+          <EmailInput />
+        </article>
+      </main>
+      <aside className="right-side" />
+    </div>
   )
 }
 
