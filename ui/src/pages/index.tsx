65a5829530e9ae62be64fdb53d8dee5549a253e5
Giovanna Blasco <giovanna.martin@opus-software.com.br>
2022-06-22 23:19:43 -0300

    Refactored to use css grid

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -7,17 +7,15 @@ import "@fontsource/josefin-sans";
 const IndexPage = () => {
   return (
     <div className="container">
-      <main className="left-side">
-        <title>Coming soon</title>
-        <article>
-          <img src={logo} alt="Brand logo" />
-          <h1><span className="title brown">We're</span> <span className="title black">coming soon</span></h1>
-          <p>Hello fellow shoppers! We're currently building our new fashion store. 
-          Add your email below to stay up-to-date with announcements and our launch deals.</p>
-          <EmailInput />
-        </article>
-      </main>
-      <aside className="right-side" />
+      <title>Coming soon</title>
+      <img src={logo} className="logo" alt="Brand logo" />
+      <article>
+        <h1><span className="title brown">We're</span> <span className="title black">coming soon</span></h1>
+        <p>Hello fellow shoppers! We're currently building our new fashion store. 
+        Add your email below to stay up-to-date with announcements and our launch deals.</p>
+        <EmailInput />
+      </article>
+      <aside className="banner" />
     </div>
   )
 }
